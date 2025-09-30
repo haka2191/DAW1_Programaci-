96f09de288bb90beef0380ae534eaf4518ac0563
haka2191 <ha2191ka@gmail.com>
2025-09-30 11:19:10 +0200

    done

--- A1/ejerciciEnClase/Ex3/src/ex10parte2.java
+++ A1/ejerciciEnClase/Ex3/src/ex10parte2.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class ex10 {
+public class ex10parte2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        /*Programa que calcula el salari net mensual d'un treballador 
-        en funció del nombre d'hores treballades i la taxa d'impostos d'acord 
-        amb les hipòtesis següents:
+        /*Programa que calcula el salari net mensual d'un treballador
+         en funció del nombre d'hores treballades i la taxa d'impostos d'acord 
+         amb les hipòtesis següents:
         Les primeres 130 hores es paguen a tarifa normal (15,00 €/h).
         Les hores que passin de 130 es paguen a 1,5 vegades la tarifa normal.
         Les taxes d'impostos són:
-        Els 500 primers euros són lliures d'impostos.
+        Els 1000 primers euros són lliures d'impostos.
         Els 400 següents tenen un 25% d'impostos.
         La resta un 45% d'impostos. Escriure nom, salari brut, taxes i salari net.
         */
@@ -21,7 +21,7 @@ public class ex10 {
         int hora;
         double salariBrut; //Salari abans d'impostos
         //Calcular salari brut
-        if (horesTreballades >= 130) {
+        if (horesTreballades >= 130) {  
             salariBrut = horesTreballades * TARIFA;
             System.out.println("El salari brut és: " + salariBrut + " euros");
         } else {
@@ -30,6 +30,6 @@ public class ex10 {
         }
         //mostrar salari brut
         System.out.println("El salari brut és: " + salariBrut + " euros");
-
+        
     }
 }
