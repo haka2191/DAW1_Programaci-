ca252e909d12189d3775b083e074c3935401ce34
haka2191 <ha2191ka@gmail.com>
2025-10-10 13:02:30 +0200

    Done Brooooo

--- A1/Bucle/Bucle/src/Ex1.java
+++ A1/Bucle/Bucle/src/Ex1.java
@@ -4,7 +4,6 @@ public class Ex1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //Te pide un numero Ejemplo 5 te sale 0 a 5 
-        //Despues te pide un numero Ejemplo 1 te sale 10 a 1
 
         System.out.print("Dime un numero 1 hasta el que quieres contar: ");
         int num1 = sc.nextInt();
@@ -12,15 +11,20 @@ public class Ex1 {
         while (contador <= num1) {
             System.out.println(contador);
             contador = contador + 1;
-            System.out.println("-> " + contador);
+            //Opcional
+            //System.out.println("-> " + contador);
         }
+        
+        //Despues te pide un numero Ejemplo 1 te sale 10 a 1
         System.out.print("Dime un numero 2 hasta el que quieres contar hacia atras: ");
         int num2 = sc.nextInt();
         contador = num2;
-        while (contador >= 0) {//contador es el numero 2 y te va restando hasta llegar a 0
+        do { //contador empieza en num2
             System.out.println(contador);
             contador = contador - 1;
-            System.out.println("-> " + contador);
-        }
+            //Opcional
+            //System.out.println("-> " + contador);
+        }while (contador >= 0);
+        //}while (!(contador <= 0));
 }
 }
