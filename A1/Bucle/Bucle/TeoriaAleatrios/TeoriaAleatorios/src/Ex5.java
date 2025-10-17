fc38c80166acc98deb811c9ece25e8f8a9a4d07f
haka2191 <ha2191ka@gmail.com>
2025-10-17 11:18:42 +0200

    modified

---
+++ A1/Bucle/Bucle/TeoriaAleatrios/TeoriaAleatorios/src/Ex5.java
@@ -0,0 +1,15 @@
+public class Ex5 {
+    public static void main(String[] args) {
+        /**
+         * Realiza un programa que lea un número positivo N y calcule y 
+         * visualice su factorial N!
+         * Siendo el factorial:
+         * 0! = 1
+         * 1! = 1
+         * 2! = 2 * 1
+         * 3! = 3 * 2* 1
+         * N! = N * (N-1) * (N-2)........* 3*2*1
+         */
+        
+    }
+}
