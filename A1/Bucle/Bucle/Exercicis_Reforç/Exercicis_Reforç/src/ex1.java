227c765a8edae8d348f2544ad821bd114184e118
haka2191 <ha2191ka@gmail.com>
2025-10-17 12:48:45 +0200

    ex1

--- A1/Bucle/Bucle/Exercicis_Reforç/Exercicis_Reforç/src/ex1.java
+++ A1/Bucle/Bucle/Exercicis_Reforç/Exercicis_Reforç/src/ex1.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ex1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /**
+         * Escriu un programa que mostri per pantalla 
+         * els 10 múltiples de 7 a partir del numero que indiqui el usuari.
+         */
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introdueix un número per començar: ");
+        int numero = sc.nextInt();
+        
+        System.out.println("Els 10 múltiples de 7 a partir de " + numero + " són:");
+        for (int i = 0; i < 10; i++) {
+            int multiple = numero + i * 7;
+            System.out.println(multiple);
+        }
+        
+        System.out.println("-----------De Profe----------------");
+        System.out.print("Introdueix un número per començar: ");
+        int numero2 = sc.nextInt();
+        int contador_multi=0;
+
+        do{
+            if (numero2 %7==0) {
+                contador_multi++;
+            }
+            numero2++;
+        }while(contador_multi<10);
+        sc.close();
     }
 }
---
+++ A1/Bucle/Bucle/Exercicis_Reforç/Exercicis_Reforç/src/ex2.java
@@ -0,0 +1,11 @@
+public class ex2 {
+    public static void main(String[] args) {
+        /**
+         * Pide un programa que pida introducir Pin hasta 3 veces: Simularemos
+         * la entrada de un portal de un banco donde te pide un pin (el pin sera 1234), 
+         * te ha de pedir el pin hasta que adivinar el pin, o al tercer
+         * golpe tiene terminar diciendo que se ha sobrepasado el número máximo de intentos.
+         */
+        
+    }
+}
