eb33ec076f87874738cf8325c42a59e6ebda7c28
haka2191 <ha2191ka@gmail.com>
2025-10-17 12:14:03 +0200

    Done

--- A1/Bucle/Bucle/TeoriaAleatrios/TeoriaAleatorios/src/Ex5.java
+++ A1/Bucle/Bucle/TeoriaAleatrios/TeoriaAleatorios/src/Ex5.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex5 {
     public static void main(String[] args) {
         /**
@@ -10,6 +13,20 @@ public class Ex5 {
          * 3! = 3 * 2* 1
          * N! = N * (N-1) * (N-2)........* 3*2*1
          */
+        Random rd = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Que factorial quieres saber ");
+        int numero = sc.nextInt();
+        int factorial=0;
+        int resultado =0;
+
+        for(int i= 1; i<= numero; i++){//asi obtendre el 1,2,3,4,5
+            factorial = i;
+            resultado = i * (i-1);
+            System.out.println(i  + " X " + (i-1) + "  " + resultado);
+        }
+        System.out.println("Factorial -> " + resultado );
+
         
     }
 }
