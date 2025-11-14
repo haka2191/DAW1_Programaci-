bf52403c423ddeee2aa780f6d21a71fe20866a57
haka2191 <ha2191ka@gmail.com>
2025-11-14 13:30:51 +0100

    13.30

--- Programacion_Java/A2/141125/141125/src/Llibreries.java
+++ Programacion_Java/A2/141125/141125/src/Llibreries.java
@@ -1,3 +1,25 @@
 public class Llibreries {
+    public static void main(String[] args) {
+        //Llibreria Math
+        double numeroPi= Math.PI;
+        double calculo = numeroPi * 2;
+        System.out.println(numeroPi + " - " + calculo);
+        double numero = -7;
+        double numero_absoluto = Math.abs(numero);
+        System.out.println(numero_absoluto);
 
+        /**
+         * Devuelve el menor valor de tipo double (más cercano a infinito negativo) 
+         * que sea mayor o igual que el argumento y sea igual a un entero.
+         */
+        double redondeo = Math.ceil(9.2); //arrodonir cap amunt
+        System.out.println(redondeo);
+
+        /**
+         * Devuelve el mayor valor de tipo double (más cercano a infinito positivo)
+         * que sea menor o igual que el argumento y sea igual a un entero.
+         */
+        double redondeo2 = Math.floor(9.6); 
+        System.out.println(redondeo2);
+    }
 }
