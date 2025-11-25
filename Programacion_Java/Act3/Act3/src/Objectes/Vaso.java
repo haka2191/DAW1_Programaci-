b2f2bd5ca5ccb7341f770e38dda2c52bb660c657
haka2191 <ha2191ka@gmail.com>
2025-11-25 10:13:00 +0100

    Act3

---
+++ Programacion_Java/Act3/Act3/src/Objectes/Vaso.java
@@ -0,0 +1,72 @@
+package Objectes;
+public class Vaso {
+    //private /public
+    private int ml_liquido;    
+    private int ml_capacidad;
+    private String material; 
+//    int cm_alto;
+//    double base;
+    public Vaso()
+    {
+        ml_capacidad = 8;
+        ml_liquido = 0;
+        material = "Plástico";
+    }
+
+     public int getMlLiquido()
+    {
+        return ml_liquido;
+    }
+
+    public int getCapacidad()
+    {
+        return ml_capacidad;
+    }
+
+     public String getMaterial()
+    {
+        return material;
+    }
+
+    public void rellenar (int nuevo_ml)
+    {
+        if (nuevo_ml < 0) {
+            System.out.println("No puedo añadir.");
+        }
+        else if ((ml_liquido + nuevo_ml) > ml_capacidad) {
+            System.out.println("Sobran ml " + (ml_capacidad - ml_liquido - nuevo_ml));
+            ml_liquido = ml_capacidad;
+        }
+        else
+        {
+            ml_liquido += nuevo_ml;
+        }
+    }
+
+    public int verter()
+    {
+        int LiquidoVertido = 8;
+
+        if (ml_liquido > LiquidoVertido) {
+            ml_liquido = ml_liquido - 2;
+            return ml_liquido;
+        }
+
+        if (ml_liquido == LiquidoVertido) {
+            ml_liquido = ml_liquido - LiquidoVertido;
+        } return ml_liquido;
+    } 
+    /**
+     * Metodo que me indica si el vaso esta lleno
+     * devuele true si esta lleno o false si no 
+     */
+
+    public boolean isLleno(){
+        return ml_liquido == ml_capacidad;
+        /*if (ml_liquido == ml_capacidad){
+            return true;
+        } else {
+            return false; 
+        }*/
+    }
+}
---
+++ Programacion_Java/Act3/Act3/teoriapoo/TeoriaPOO.java
@@ -0,0 +1,65 @@
+import Objectes.Botella;
+import Objectes.Vaso;
+public class TeoriaPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Botella bottle;
+        bottle = new Botella();
+        System.out.println("capacida botella " + bottle.getCapacidad());
+        System.out.println("ml liquido: " + bottle.getMlLiquido());
+        System.out.println("Material: " + bottle.getMaterial());
+        System.out.println("tapon " + bottle.estaCerrada());
+        
+        System.out.println("Abro bottella");
+        bottle.abrir();
+        System.out.println("Intento poner 70");
+        bottle.setLiquido(70);
+        System.out.println("ml liquido: " + bottle.getMlLiquido());
+        
+        System.out.println("Intento poner 10");
+        bottle.setLiquido(10);
+        System.out.println("ml liquido: " + bottle.getMlLiquido());
+        System.out.println("Intento poner 5");
+        bottle.setLiquido(5);
+        System.out.println("ml liquido: " + bottle.getMlLiquido());
+        System.out.println("relleno con 5");
+        bottle.rellenar(5);
+        System.out.println("ml liquido: " + bottle.getMlLiquido());
+        
+        Vaso glass1 = new Vaso();
+        Vaso glass2 = new Vaso();
+        Vaso glass3 = new Vaso();
+        Vaso glass4 = new Vaso();
+        System.out.println("Llena la botella");
+        bottle.rellenar(25);
+
+        System.out.println("Empiezo a llenar vaosos con la botella" + bottle.getMlLiquido());
+        while (!bottle.isVacia()) {
+            //if (glass1.getMlLiquido()!=glass1.getCapacidad()) {
+            if (!glass1.isLleno()) {
+                System.out.println("Rellenando vaso 1");
+                glass1.rellenar(bottle.verter());
+                System.out.println("A la botella le queda " + bottle.getMlLiquido());
+            }else if (!glass2.isLleno()) {
+                System.out.println("Rellenando vaso 2");
+                glass2.rellenar(bottle.verter());
+                System.out.println("A la botella le queda " + bottle.getMlLiquido());
+
+            }else if (!glass3.isLleno()) {
+                System.out.println("Rellenando vaso 3");
+                glass3.rellenar(bottle.verter());
+                System.out.println("A la botella le queda " + bottle.getMlLiquido());
+            }else if (!glass4.isLleno()) {
+                System.out.println("Rellenando vaso 4");
+                glass4.rellenar(bottle.verter());
+                System.out.println("A la botella le queda " + bottle.getMlLiquido());
+            } else {
+                System.out.println("Todos los vasos llenos");
+                break;
+            }
+        }
+    }
+}
